c00cb99a2f86396c4cba3fa528d51af2484ecb6f
unknown <dethgrenade@gmail.com>
2020-10-16 13:57:51 -0700

    added more hero ability details to seed files

--- data/seeds/006-hero-abilities.js
+++ data/seeds/006-hero-abilities.js
@@ -1,171 +1,188 @@
-
 exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('abilities').truncate()
+  return knex("abilities")
+    .truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('abilities').insert([
+      return knex("abilities").insert([
         {
-          name: 'Dragon Strike',
-          description: 'Unleash a dragon spirit that knocks back enemies in its path and deal massive Physical Damage',
-          attributes: 'none',
-          hero_id: 1
+          name: "Dragon Strike",
+          description:
+            "Unleash a dragon spirit that knocks back enemies in its path and deal massive Physical Damage",
+          attributes: "none",
+          hero_id: 1,
         },
         {
-          name: 'Uppercut',
-          description: 'Cleave a nearby enemy, dealing Physical Damage as well as Lifting and Stunning them',
-          attributes: 'none',
-          hero_id: 1
+          name: "Uppercut",
+          description:
+            "Cleave a nearby enemy, dealing Physical Damage as well as Lifting and Stunning them",
+          attributes: "none",
+          hero_id: 1,
         },
         {
-          name: 'Parry',
-          description: 'Whenever Elric takes Damage, if he is not currently using an ability, he will Parry the attack, reducing the amount of Damage he takes. He will then immediately counterattack',
-          attributes: 'none',
-          hero_id: 1
+          name: "Parry",
+          description:
+            "Whenever Elric takes Damage, if he is not currently using an ability, he will Parry the attack, reducing the amount of Damage he takes. He will then immediately counterattack",
+          attributes: "none",
+          hero_id: 1,
         },
         {
-          name: 'Knight\'s Oath',
-          description: 'Take an oath of loyalty and increase Health',
-          attributes: 'none',
-          hero_id: 1
+          name: "Knight's Oath",
+          description: "Take an oath of loyalty and increase Health",
+          attributes: "none",
+          hero_id: 1,
         },
         {
-          name: 'Engraved Blade',
-          description: 'A runic incription appears on Elric\'s sword in one of three colors, bringing and additional effect to all of his abilities. Green grants additional Damage based on the enemy\'s Max Health, Red grants a Lifesteal effect and Yellow reduces enemy Energy',
-          attributes: 'none',
-          hero_id: 1
+          name: "Engraved Blade",
+          description:
+            "A runic incription appears on Elric's sword in one of three colors, bringing and additional effect to all of his abilities. Green grants additional Damage based on the enemy's Max Health, Red grants a Lifesteal effect and Yellow reduces enemy Energy",
+          attributes: "none",
+          hero_id: 1,
         },
         {
-          name: 'x',
-          description: 'x',
-          attributes: 'x',
-          hero_id: 2
+          name: "Fearless Charge",
+          description:
+            "Tareth knocks an enemy into the air, then pummels them with a series of Physical Attacks. If the enemy dies during the course of her Ultimate, Tareth gets a large boost in Physical Attack",
+          attributes: "none",
+          hero_id: 2,
         },
         {
-          name: 'x',
-          description: 'x',
-          attributes: 'x',
-          hero_id: 2
+          name: "Arrowstorm",
+          description:
+            "Tareth unleashes a hail of flaming arrows, dealing AoE Physical Damage. The more enemies are struck by arrows, the more Tareth's allies receive a boost to Attack Speed and Movement Speed",
+          attributes: "fire",
+          hero_id: 2,
         },
         {
-          name: 'x',
-          description: 'x',
-          attributes: 'x',
-          hero_id: 2
+          name: "Knight's Protection",
+          description:
+            "Tareth increases the Lifesteal rating of a teammate or herself, as well as restoring some of their Health",
+          attributes: "none",
+          hero_id: 2,
         },
         {
-          name: 'x',
-          description: 'x',
-          attributes: 'x',
-          hero_id: 2
+          name: "Divine Courage",
+          description:
+            "Tareth's teammates are encouraged by her fearlessness and gain an increase in Physical Armor",
+          attributes: "none",
+          hero_id: 2,
         },
         {
-          name: 'x',
-          description: 'x',
-          attributes: 'x',
-          hero_id: 3
+          name: "Arcane Whirlwind",
+          description:
+            "Jasper summons a variety of small items that rapidly revolve around the enemy party, dealing Magic Damage to all enemies. In addition, when enemies are hit by an item, they will be marked based on the type of item that hit them. When two enemies with the same mark get close to each other, the marks will disappear and they will both take Magic Damage plus an additional effect based on the type of mark. If marked with a bell, they will lose Energy. If marked with a fishbone, they will take Damage. If marked with a riceball, they will be Stunned. Jasper's Basic Attack may also randomly mark an enemy. Jasper will never be affected if a mark is placed upon him",
+          attributes: "none",
+          hero_id: 3,
         },
         {
-          name: 'x',
-          description: 'x',
-          attributes: 'x',
-          hero_id: 3
+          name: "Precision Pounce",
+          description:
+            "Jasper flies above an enemy unit, then pounces and scratches them, dealing Magic Damage. This attack will also randomly mark the enemy",
+          attributes: "none",
+          hero_id: 3,
         },
         {
-          name: 'x',
-          description: 'x',
-          attributes: 'x',
-          hero_id: 3
+          name: "Phantom Claw",
+          description:
+            "Jasper summons a giant cat claw which grasps an enemy, then throws them to a different location on the battlefield. This attack also randomly marks the enemy",
+          attributes: "none",
+          hero_id: 3,
         },
         {
-          name: 'x',
-          description: 'x',
-          attributes: 'x',
-          hero_id: 3
+          name: "Rapid Escape",
+          description:
+            "Jasper increases his Dodge Rating, as well as escaping to the protection of his own party whenever he takes Damage equivalent to 10% of his Health or is hit 5 times",
+          attributes: "none",
+          hero_id: 3,
         },
         {
-          name: 'x',
-          description: 'x',
-          attributes: 'x',
-          hero_id: 4
+          name: "Righteous Wrath",
+          description:
+            "Plasma rains down from the sky delivering intense AoE Magic Damage to one or more enemies",
+          attributes: "none",
+          hero_id: 4,
         },
         {
-          name: 'x',
-          description: 'x',
-          attributes: 'x',
-          hero_id: 4
+          name: "Blazing Javelins",
+          description:
+            "Summon 4 glowing spears which hurl themselves at the enemy dealing Magic Damage to any in their path",
+          attributes: "none",
+          hero_id: 4,
         },
         {
-          name: 'x',
-          description: 'x',
-          attributes: 'x',
-          hero_id: 4
+          name: "Luminous Blast",
+          description:
+            "Generate a blast of light which is guaranteed to Slow enemies within range as well as well as dealing Magic Damage",
+          attributes: "none",
+          hero_id: 4,
         },
         {
-          name: 'x',
-          description: 'x',
-          attributes: 'x',
-          hero_id: 4
+          name: "Radiant Rune",
+          description:
+            "Serafine draws an ancient rune with her wand, casting a spell which Silences a single enemy and greatly reduces their Magic Armor for 4 seconds",
+          attributes: "none",
+          hero_id: 4,
         },
         {
-          name: 'x',
-          description: 'x',
-          attributes: 'x',
-          hero_id: 4
+          name: "Shimmering Glow",
+          description:
+            "Every time Serafine deals Damage, her Magic Attack will be increased. It can be increased up to 50 times per wave",
+          attributes: "none",
+          hero_id: 4,
         },
         {
-          name: 'x',
-          description: 'x',
-          attributes: 'x',
-          hero_id: 5
+          name: "none",
+          description: "none",
+          attributes: "none",
+          hero_id: 5,
         },
         {
-          name: 'x',
-          description: 'x',
-          attributes: 'x',
-          hero_id: 5
+          name: "none",
+          description: "none",
+          attributes: "none",
+          hero_id: 5,
         },
         {
-          name: 'x',
-          description: 'x',
-          attributes: 'x',
-          hero_id: 5
+          name: "none",
+          description: "none",
+          attributes: "none",
+          hero_id: 5,
         },
         {
-          name: 'x',
-          description: 'x',
-          attributes: 'x',
-          hero_id: 5
+          name: "none",
+          description: "none",
+          attributes: "none",
+          hero_id: 5,
         },
         {
-          name: 'x',
-          description: 'x',
-          attributes: 'x',
-          hero_id: 5
+          name: "none",
+          description: "none",
+          attributes: "none",
+          hero_id: 5,
         },
         {
-          name: 'x',
-          description: 'x',
-          attributes: 'x',
-          hero_id: 6
+          name: "none",
+          description: "none",
+          attributes: "none",
+          hero_id: 6,
         },
         {
-          name: 'x',
-          description: 'x',
-          attributes: 'x',
-          hero_id: 6
+          name: "none",
+          description: "none",
+          attributes: "none",
+          hero_id: 6,
         },
         {
-          name: 'x',
-          description: 'x',
-          attributes: 'x',
-          hero_id: 6
+          name: "none",
+          description: "none",
+          attributes: "none",
+          hero_id: 6,
         },
         {
-          name: 'x',
-          description: 'x',
-          attributes: 'x',
-          hero_id: 6
+          name: "none",
+          description: "none",
+          attributes: "none",
+          hero_id: 6,
         },
       ]);
     });
--- data/seeds/007-ability-stats.js
+++ data/seeds/007-ability-stats.js
@@ -1,165 +1,247 @@
-
 exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('ability_stats').truncate()
+  return knex("ability_stats")
+    .truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('ability_stats').insert([
+      return knex("ability_stats").insert([
         {
-          stat_details: '(+ 10 Damage per Ability Point)',
-          ability_id: 1
+          stat_details: "(+ 10 Damage per Ability Point)",
+          ability_id: 1,
         },
         {
-          stat_details: 'Level 100: Deals a base of 1030 Damage',
-          ability_id: 1
+          stat_details: "Level 100: Deals a base of 1030 Damage",
+          ability_id: 1,
         },
         {
-          stat_details: 'Knock-back Success Rate increases with ability level enemies under level 100 will be knocked back every time',
-          ability_id: 1
+          stat_details:
+            "Knock-back Success Rate increases with ability level enemies under level 100 will be knocked back every time",
+          ability_id: 1,
         },
         {
-          stat_details: '(+ 16 Damage per Ability Point)',
-          ability_id: 2
+          stat_details: "(+ 16 Damage per Ability Point)",
+          ability_id: 2,
         },
         {
-          stat_details: 'Level 100: Deals a base of 1600 Damage',
-          ability_id: 2
+          stat_details: "Level 100: Deals a base of 1600 Damage",
+          ability_id: 2,
         },
         {
-          stat_details: 'Lift Success Rate increases with ability level enemies below level 100 will be Lifted every time',
-          ability_id: 2
+          stat_details:
+            "Lift Success Rate increases with ability level enemies below level 100 will be Lifted every time",
+          ability_id: 2,
         },
         {
-          stat_details: '(Base 280 + 14 Damage per Ability Point)',
-          ability_id: 3
+          stat_details: "(Base 280 + 14 Damage per Ability Point)",
+          ability_id: 3,
         },
         {
-          stat_details: 'Level 80: Deals a base of 1400 Damage',
-          ability_id: 3
+          stat_details: "Level 80: Deals a base of 1400 Damage",
+          ability_id: 3,
         },
         {
-          stat_details: '(Base 2000 + 50 Health per Ability Point)',
-          ability_id: 4
+          stat_details: "(Base 2000 + 50 Health per Ability Point)",
+          ability_id: 4,
         },
         {
-          stat_details: 'Level 60: Passive: Increase Health by 5000',
-          ability_id: 4
+          stat_details: "Level 60: Passive: Increase Health by 5000",
+          ability_id: 4,
         },
         {
-          stat_details: '(Green: Base 12.3% + 0.3% and 3660 + 60 Damage per Ability Point)',
-          ability_id: 5
+          stat_details:
+            "(Green: Base 12.3% + 0.3% and 3660 + 60 Damage per Ability Point)",
+          ability_id: 5,
         },
         {
-          stat_details: '(Red: Base 305 + 5 Lifesteal per Ability Point)',
-          ability_id: 5
+          stat_details: "(Red: Base 305 + 5 Lifesteal per Ability Point)",
+          ability_id: 5,
         },
         {
-          stat_details: '(Yellow: Base 152.5 + 2.5 Energy per Ability Point)',
-          ability_id: 5
+          stat_details: "(Yellow: Base 152.5 + 2.5 Energy per Ability Point)",
+          ability_id: 5,
         },
         {
-          stat_details: 'Level 40: Green: Increases Damage by 30% of target\'s Max Health',
-          ability_id: 5
+          stat_details:
+            "Level 40: Green: Increases Damage by 30% of target's Max Health",
+          ability_id: 5,
         },
         {
-          stat_details: 'Deals a max of 6000 Damage',
-          ability_id: 5
+          stat_details: "Deals a max of 6000 Damage",
+          ability_id: 5,
         },
         {
-          stat_details: 'Red: Increases Lifesteal by 500',
-          ability_id: 5
+          stat_details: "Red: Increases Lifesteal by 500",
+          ability_id: 5,
         },
         {
-          stat_details: 'Yellow: Reduces target\'s Energy by 250',
-          ability_id: 5
+          stat_details: "Yellow: Reduces target's Energy by 250",
+          ability_id: 5,
         },
         {
-          stat_details: 'x',
-          ability_id: 6
+          stat_details:
+            "(+ 15 Damage and + 20 Physical Attack per Ability Point)",
+          ability_id: 6,
         },
         {
-          stat_details: 'x',
-          ability_id: 7
+          stat_details: "Level 100: Deals a base of 1500 Damage",
+          ability_id: 6,
         },
         {
-          stat_details: 'x',
-          ability_id: 8
+          stat_details:
+            "Physical Attack increases by 2000 when enemy is killed by Ultimate",
+          ability_id: 6,
         },
         {
-          stat_details: 'x',
-          ability_id: 9
+          stat_details:
+            "(+ 10 Damage and + 0.06% Movement and Attack Speed per Ability Point)",
+          ability_id: 7,
         },
         {
-          stat_details: 'x',
-          ability_id: 10
+          stat_details: "Level 100: Deals a base of 1000 Damage",
+          ability_id: 7,
         },
         {
-          stat_details: 'x',
-          ability_id: 11
+          stat_details:
+            "Every enemy struck increases Movement and Attack Speed by 6%",
+          ability_id: 7,
         },
         {
-          stat_details: 'x',
-          ability_id: 12
+          stat_details:
+            "(Base 103 + 5 Health and Base 61 + 3 Lifesteal per Ability Point)",
+          ability_id: 8,
         },
         {
-          stat_details: 'x',
-          ability_id: 13
+          stat_details:
+            "Level 80: Restores 503 Health per second for 8 seconds",
+          ability_id: 8,
         },
         {
-          stat_details: 'x',
-          ability_id: 14
+          stat_details: "Lifesteal rating increased by 301",
+          ability_id: 8,
         },
         {
-          stat_details: 'x',
-          ability_id: 15
+          stat_details: "(Base 80 + 2 Physical Armor per Ability Point)",
+          ability_id: 9,
         },
         {
-          stat_details: 'x',
-          ability_id: 16
+          stat_details:
+            "Level 60: Aura: All allies' Physical Armor increased by 200",
+          ability_id: 9,
         },
         {
-          stat_details: 'x',
-          ability_id: 17
+          stat_details: "(+ 8 Damage per Ability Point)",
+          ability_id: 10,
         },
         {
-          stat_details: 'x',
-          ability_id: 18
+          stat_details: "Level 100: Deals a base of 800 Damage",
+          ability_id: 10,
         },
         {
-          stat_details: 'x',
-          ability_id: 19
+          stat_details: "(+ 5 Damage per Ability Point)",
+          ability_id: 11,
         },
         {
-          stat_details: 'x',
-          ability_id: 20
+          stat_details: "Level 100: Deals a base of 500 Damage",
+          ability_id: 11,
         },
         {
-          stat_details: 'x',
-          ability_id: 21
+          stat_details: "(Base 200 + 10 Damage per Ability Point)",
+          ability_id: 12,
         },
         {
-          stat_details: 'x',
-          ability_id: 22
+          stat_details: "Level 80: Deals a base of 1000 Damage",
+          ability_id: 12,
         },
         {
-          stat_details: 'x',
-          ability_id: 23
+          stat_details: "(Base 80 + 2 Dodge per Ability Point)",
+          ability_id: 13,
         },
         {
-          stat_details: 'x',
-          ability_id: 24
+          stat_details: "Level 60: Passive: Increases Dodge Rating by 200",
+          ability_id: 13,
         },
         {
-          stat_details: 'x',
-          ability_id: 25
+          stat_details: "(Base 72 + 12 Damage per Ability Point)",
+          ability_id: 14,
         },
         {
-          stat_details: 'x',
-          ability_id: 26
+          stat_details: "Level 100: Each wave deals a base of 1272 Damage",
+          ability_id: 14,
         },
         {
-          stat_details: 'x',
-          ability_id: 27
+          stat_details: "(+ 6 Damage per Ability Point)",
+          ability_id: 15,
+        },
+        {
+          stat_details: "Level 100: Deals a base of 600 Damage",
+          ability_id: 15,
+        },
+        {
+          stat_details: "(Base 200 + 10 Damage per Ability Point)",
+          ability_id: 16,
+        },
+        {
+          stat_details: "Level 80: Deals a base of 1000 Damage",
+          ability_id: 16,
+        },
+        {
+          stat_details: "(Base 80 + 2 Magic Armor per Ability Point)",
+          ability_id: 17,
+        },
+        {
+          stat_details:
+            "Level 60: Reduce target's Magic Armor by 200 for 4 seconds",
+          ability_id: 17,
+        },
+        {
+          stat_details:
+            "Silence Success Rate increases with ability level. Enemies under level 100 will be Silenced every time",
+          ability_id: 17,
+        },
+        {
+          stat_details: "(Base 30 + 0.5 Magic Attack per Ability Point)",
+          ability_id: 18,
+        },
+        {
+          stat_details:
+            "Level 40: Magic Attack increased by 50 each time Serafine deals Damage",
+          ability_id: 18,
+        },
+        {
+          stat_details: "x",
+          ability_id: 19,
+        },
+        {
+          stat_details: "x",
+          ability_id: 20,
+        },
+        {
+          stat_details: "x",
+          ability_id: 21,
+        },
+        {
+          stat_details: "x",
+          ability_id: 22,
+        },
+        {
+          stat_details: "x",
+          ability_id: 23,
+        },
+        {
+          stat_details: "x",
+          ability_id: 24,
+        },
+        {
+          stat_details: "x",
+          ability_id: 25,
+        },
+        {
+          stat_details: "x",
+          ability_id: 26,
+        },
+        {
+          stat_details: "x",
+          ability_id: 27,
         },
       ]);
     });
