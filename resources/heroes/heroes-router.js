e47e87f92f4c7f503e1211fd9629c349764b1e7d
unknown <dethgrenade@gmail.com>
2020-12-01 11:06:31 -0800

    added route to post new Heroes to db

--- resources/heroes/heroes-router.js
+++ resources/heroes/heroes-router.js
@@ -1,36 +1,47 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
 //Custom Middleware
-const { validateHero } = require('./heroes-service')
+const { validateHero } = require("./heroes-service");
 
 //Database connection
-const Heroes = require('./heroes-model')
+const Heroes = require("./heroes-model");
 
-router.get('/', (req, res) => {
-    Heroes.find()
-        .then(heroes => {
-            if (heroes.length) {
-                res.status(200).json(heroes)
-            }
-            else {
-                res.status(404).json({ message: 'No heroes found' })
-            }
-        })
-        .catch(error => {
-            res.status(500).json({ error: error.message })
-        })
-})
+router.post("/", (req, res) => {
+  const { hero } = req.body;
 
-router.get('/:hero', validateHero, (req, res) => {
-    const { hero } = req.params
+  Heroes.add(hero)
+    .then((hero) => {
+      res.status(200).json(hero);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
+});
 
-    Heroes.findBy({ name: hero })
-        .then(hero => {
-            res.status(200).json(hero)
-        })
-        .catch(error => {
-            res.status(500).json({ error: error.message })
-        })
-})
+router.get("/", (req, res) => {
+  Heroes.find()
+    .then((heroes) => {
+      if (heroes.length) {
+        res.status(200).json(heroes);
+      } else {
+        res.status(404).json({ message: "No heroes found" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
+});
 
-module.exports = router
+router.get("/:hero", validateHero, (req, res) => {
+  const { hero } = req.params;
+
+  Heroes.findBy({ name: hero })
+    .then((hero) => {
+      res.status(200).json(hero);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
+});
+
+module.exports = router;
